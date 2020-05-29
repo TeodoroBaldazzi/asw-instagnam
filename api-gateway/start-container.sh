aa4b698832dbd2407b9f4e878518316c7c1ee105
micol <mik.cervero@gmail.com>
2020-05-29 21:07:15 +0200

    update sh

--- api-gateway/start-container.sh
+++ api-gateway/start-container.sh
@@ -5,5 +5,5 @@ echo Starting Api-gateway in a Docker Container
 kubectl apply -f Deployment-api-gateway.yaml
 
 kubectl apply -f Service-api-gateway.yaml
-
-kubectl port-forward --namespace=asw-project $(kubectl get pods --namespace=asw-project -l=app=api-gateway | awk '{print $1}' | grep api-gateway) 7000:8080
+                 
+kubectl port-forward --namespace=asw-project $(kubectl get pods --namespace=asw-project -l=app=api-gateway | awk '{print $1}' | grep api-gateway) 8080:8080
