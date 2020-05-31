564fba35de24dff77a7771d703e39a2764e50e46
micol <mik.cervero@gmail.com>
2020-05-31 13:14:05 +0200

    add sleep command

--- api-gateway/start-container.sh
+++ api-gateway/start-container.sh
@@ -5,5 +5,9 @@ echo Starting Api-gateway in a Docker Container
 kubectl apply -f Deployment-api-gateway.yaml
 
 kubectl apply -f Service-api-gateway.yaml
-                 
-kubectl port-forward --namespace=asw-project $(kubectl get pods --namespace=asw-project -l=app=api-gateway | awk '{print $1}' | grep api-gateway) 8080:8080
+ 
+sleep 10s
+ 
+kubectl port-forward --namespace=asw-project $(kubectl get pods --namespace=asw-project -l=app=api-gateway | grep api-gateway | awk '{print $1}') 8080:8080
+ 
+
