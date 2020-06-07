4aa27044e6acc6388e0a2234d424ffdeefa6321f
TEODORO BALDAZZI <teo.baldazzi@stud.uniroma3.it>
2020-06-07 19:11:19 +0200

    Removed warning

--- ricette/src/main/java/asw/instagnam/commons/domain/RicetteService.java
+++ ricette/src/main/java/asw/instagnam/commons/domain/RicetteService.java
@@ -2,7 +2,6 @@ package asw.instagnam.commons.domain;
 
 import asw.instagnam.common.event.RicettaCreatedEvent;
 import asw.instagnam.commons.producer.RicetteEventProducer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
