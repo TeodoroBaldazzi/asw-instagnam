5a9670c12fbfd563c4e6d0927a9af42286e6493e
Stefano Sferrazza <ste.sferrazza@stud.uniroma3.it>
2020-06-10 17:29:33 +0200

    added clean of persistent volumes on delete.sh

--- deploy/postgres-ricette-seguite/delete.sh
+++ deploy/postgres-ricette-seguite/delete.sh
@@ -4,3 +4,4 @@ echo Deleting Postgres Ricette
 
 kubectl delete service/postgres-ricette-seguite-service -n asw-project
 kubectl delete deployment postgres-ricette-seguite-deployment -n asw-project
+kubectl delete pv postgres-ricette-seguite-pv-volume
--- deploy/postgres-ricette/delete.sh
+++ deploy/postgres-ricette/delete.sh
@@ -4,3 +4,4 @@ echo Deleting Postgres Ricette
 
 kubectl delete service/postgres-ricette-service -n asw-project
 kubectl delete deployment postgres-ricette-deployment -n asw-project
+kubectl delete pv postgres-ricette-pv-volume
