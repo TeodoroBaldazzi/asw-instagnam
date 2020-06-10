e4419d6dc6e8796d0d00fd5b761693cf727f558d
Stefano Sferrazza <ste.sferrazza@stud.uniroma3.it>
2020-06-10 15:33:36 +0200

    Independent implementation of postgres-ricette

---
+++ deploy/postgres-ricette/start-container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo Starting Postgres Microservice in a Docker Container
+
+kubectl apply -f postgres-ricette-configmap.yaml
+kubectl apply -f postgres-storage-ricette.yaml
+kubectl apply -f deployment-postgres-ricette.yaml
+kubectl apply -f service-postgres-ricette.yaml
