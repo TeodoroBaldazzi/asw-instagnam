88dd6a34b775780b3fd164b8597221b03c908e94
stefano <ste.sferrazza@stud.uniroma3.it>
2020-06-15 00:11:53 +0200

    fix

--- test.sh
+++ test.sh
@@ -8,13 +8,12 @@ echo ""
 curl http://localhost:8090/connessioni/connessioni
 echo ""
 echo ""
-echo ""
 echo "Ricette"
 echo ""
 curl http://localhost:8090/ricette/ricette
 echo ""
 echo ""
-echo ""
 echo "Ricette seguite da Gennaro"
 echo ""
 curl http://localhost:8090/ricette-seguite/ricetteseguite/Gennaro
+echo ""
