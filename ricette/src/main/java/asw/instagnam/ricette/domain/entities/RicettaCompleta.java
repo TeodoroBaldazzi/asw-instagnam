345b8072b3339bec7fc1b71010c240472e7f5f32
TEODORO BALDAZZI <teo.baldazzi@stud.uniroma3.it>
2020-06-14 21:16:16 +0200

    Ricette implemented

---
+++ ricette/src/main/java/asw/instagnam/ricette/domain/entities/RicettaCompleta.java
@@ -0,0 +1,32 @@
+package asw.instagnam.ricette.domain.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+
+
+/* Ricetta, in formato completo. */ 
+@Entity
+@IdClass(RicettaId.class)
+@Data @NoArgsConstructor
+public class RicettaCompleta {
+
+	private Long id;
+	@Id 
+	private String autore;
+	@Id
+	private String titolo; 
+	private String preparazione; 
+	
+	public RicettaCompleta(Long id, String autore, String titolo, String preparazione) {
+		this();
+		this.id = id;
+		this.autore = autore; 
+		this.titolo = titolo; 
+		this.preparazione = preparazione; 
+	}
+	
+}
